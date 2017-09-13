16d329d7b60eee361583630e787c811bdd775bca
Avinta Putri Agistasari <putriagistasari@gmail.com>
2017-09-13 20:29:09 +0700

    organizing gradle on Data and Domain module

---
+++ app/src/androidTest/java/org/codenergic/theskeleton/ExampleUnitTest.java
@@ -0,0 +1,11 @@
+package org.codenergic.theskeleton;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+}
