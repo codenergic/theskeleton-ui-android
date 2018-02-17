45cff0c497ec77088d01e3b7bbbee5c714330f23
Avinta Putri Agistasari <putriagistasari@gmail.com>
2018-02-17 21:53:54 +0700

    change color, for naming you can find in : http://chir.ag/projects/name-that-color/

--- app/src/main/java/org/codenergic/theskeleton/login/LoginActivity.java
+++ app/src/main/java/org/codenergic/theskeleton/login/LoginActivity.java
@@ -3,6 +3,7 @@ package org.codenergic.theskeleton.login;
 import org.codenergic.theskeleton.R;
 import org.codenergic.theskeleton.base.BaseActivity;
 import org.codenergic.theskeleton.base.BasePresenter;
+import org.codenergic.theskeleton.helper.AlertHelper;
 import org.codenergic.theskeleton.main.MainActivity;
 import org.codenergic.theskeleton.register.RegisterActivity;
 
@@ -15,7 +16,6 @@ import javax.inject.Inject;
 import butterknife.BindView;
 import butterknife.OnClick;
 import dagger.android.AndroidInjection;
-import timber.log.Timber;
 
 /**
  * Created by putrice on 9/22/17.
@@ -33,13 +33,13 @@ public class LoginActivity extends BaseActivity implements LoginContract.View {
     LoginPresenter presenter;
 
     @Override
-    public int getLayout() {
-        return R.layout.activity_login;
+    public void setup() {
+        AndroidInjection.inject(this);
     }
 
     @Override
-    public void setup() {
-        AndroidInjection.inject(this);
+    public int getLayout() {
+        return R.layout.activity_login;
     }
 
     @Override
@@ -67,7 +67,6 @@ public class LoginActivity extends BaseActivity implements LoginContract.View {
 
     @Override
     public void onLoginSuccess() {
-        Toast.makeText(this, "Login succeed", Toast.LENGTH_LONG).show();
         navigateToMain();
     }
 
@@ -78,8 +77,6 @@ public class LoginActivity extends BaseActivity implements LoginContract.View {
 
     @Override
     public void onLoginFailed(String errorMessage) {
-        //TODO using proper error message
-        Timber.e(errorMessage);
-        Toast.makeText(this, "Login error: " + errorMessage, Toast.LENGTH_LONG).show();
+        AlertHelper.showWarningAlert(this, errorMessage);
     }
 }
