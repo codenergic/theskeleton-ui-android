9b18faa278a1ffdffe18b13cc36c8ae1b75b141c
Avinta Putri Agistasari <putriagistasari@gmail.com>
2018-02-17 22:04:31 +0700

    change color for alert and alert for success and error

--- app/src/main/java/org/codenergic/theskeleton/helper/AlertHelper.java
+++ app/src/main/java/org/codenergic/theskeleton/helper/AlertHelper.java
@@ -1,9 +1,11 @@
 package org.codenergic.theskeleton.helper;
 
+import com.tapadoo.alerter.Alerter;
+
+import org.codenergic.theskeleton.R;
+
 import android.app.Activity;
 import android.support.v4.content.ContextCompat;
-
-import com.tapadoo.alerter.Alerter;
 
 /**
  * Created by putrice on 2/4/18.
@@ -13,9 +15,24 @@ public class AlertHelper {
     public static void showWarningAlert(Activity activity, String text) {
         Alerter.create(activity)
             .setText(text)
-            .setIcon(android.R.drawable.stat_sys_warning)
             .setBackgroundColorInt(
-                ContextCompat.getColor(activity, android.R.color.holo_orange_dark))
+                ContextCompat.getColor(activity, R.color.sunshade))
+            .show();
+    }
+
+    public static void showErrorAlert(Activity activity, String text) {
+        Alerter.create(activity)
+            .setText(text)
+            .setBackgroundColorInt(
+                ContextCompat.getColor(activity, R.color.flamingo))
+            .show();
+    }
+
+    public static void showSuccessAlert(Activity activity, String text) {
+        Alerter.create(activity)
+            .setText(text)
+            .setBackgroundColorInt(
+                ContextCompat.getColor(activity, R.color.fruitSalad))
             .show();
     }
 
